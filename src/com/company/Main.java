c3a7cb61e1b2a68ab821437a8aae787ab3a75c74
kevin <kevinindangasi1@gmail.com>
2022-03-22 16:37:42 +0300

    While and Do while loops

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 int i = 5 ;
+
+     while(i > 0)
+     {
+         System.out.println("This is the functional statement within our loop");
+        i--;
+     }
+    }
+}
